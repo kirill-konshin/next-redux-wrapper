63eb24bda1d7e1f30f9f107e5ee370a81f04c083
Kirill Konshin <kirill.konshin@gmail.com>
2017-04-17 11:02:03 -0700

    URL property

--- pages/other.js
+++ pages/other.js
@@ -3,8 +3,9 @@ import React from "react";
 import {makeStore} from "../components/store";
 import wrapper from "../src";
 
-export default wrapper(makeStore)(() => (
+export default wrapper(makeStore)(({url: {pathname}}) => (
     <div>
+        <div>Using Next.js default prop in a wrapped component: {pathname}</div>
         <nav>
             <Link href="/"><a>Navigate to index</a></Link>
         </nav>
