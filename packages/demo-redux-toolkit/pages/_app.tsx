61c5ea8b89c41639e5c64b1d34b47b55f7c562bc
Kirill Konshin <kirill.konshin@gmail.com>
2021-09-13 16:23:14 -0700

    Hooks instead of class-based component

--- packages/demo-redux-toolkit/pages/_app.tsx
+++ packages/demo-redux-toolkit/pages/_app.tsx
@@ -1,7 +1,15 @@
 import React, {FC} from 'react';
+import {Provider} from 'react-redux';
 import {AppProps} from 'next/app';
 import {wrapper} from '../store';
 
-const MyApp: FC<AppProps> = ({Component, pageProps}) => <Component {...pageProps} />;
+const MyApp: FC<AppProps> = ({Component, ...rest}) => {
+    const {store, props} = wrapper.useWrappedStore(rest);
+    return (
+        <Provider store={store}>
+            <Component {...props.pageProps} />
+        </Provider>
+    );
+};
 
-export default wrapper.withRedux(MyApp);
+export default MyApp;
