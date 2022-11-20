1187858ad6279c4d825c103841caa62a640fb6c9
voinik <victor_panteleev@hotmail.com>
2022-11-20 10:41:33 +0100

    Add detail page

---
+++ packages/demo-redux-toolkit/pages/detail/[id].tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {useDispatch, useSelector, useStore} from 'react-redux';
+import Link from 'next/link';
+import {InferGetServerSidePropsType, NextPage} from 'next';
+import {fetchDetail, selectDetailPageId, selectDetailPageStateTimestamp, selectDetailPageSummary, wrapper} from '../../store';
+
+const Page: NextPage<InferGetServerSidePropsType<typeof getServerSideProps>> = ({serverTimestamp}) => {
+    console.log('State on render', useStore().getState());
+    console.log('Timestamp on server: ', serverTimestamp);
+    const dispatch = useDispatch();
+    const pageId = useSelector(selectDetailPageId);
+    console.log('pageId: ', pageId);
+    const pageSummary = useSelector(selectDetailPageSummary);
+    console.log('pageName: ', pageSummary);
+    const stateTimestamp = useSelector(selectDetailPageStateTimestamp);
+
+    console[pageSummary ? 'info' : 'warn']('Rendered pageName: ', pageSummary);
+
+    if (!pageSummary || !pageId) {
+        // If we use the useMemo solution (solution B), then this error will throw if you navigate to a page that triggers a hydrate (e.g. a subject page),
+        // and if there is no data reconciliation in the HYDRATE reducer.
+        // The reason is useMemo causes the hydration to occur before the new page component is mounted, which means this component is still mounted
+        // while the new data is hydrated. That leads to the above selectors resolving to undefined, because the hydrate for the new page
+        // puts the slice state back to its initial state (because we don't use data reconciliation).
+        throw new Error('Whoops!');
+    }
+
+    // We use prefetch={false} to make sure that only the slice of this page is hydrated, and not other slices.
+    // Additionally, when we navigate, we want the freshest data possible, as the GSSP in the pages returns a timestamp of the request time.
+    return (
+        <>
+            <div style={{backgroundColor: 'pink', padding: '20px'}}>Timestamp on server: {serverTimestamp}</div>
+            <div style={{backgroundColor: 'lavender', padding: '20px'}}>Timestamp in state: {stateTimestamp}</div>
+            <div className={`page${pageId}`}>
+                <h3>{pageSummary}</h3>
+                <Link href="/subject/1" prefetch={false}>
+                    Go id=1
+                </Link>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <Link href="/subject/2" prefetch={false}>
+                    Go id=2
+                </Link>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <Link href="/detail/1" prefetch={false}>
+                    Go to details id=1
+                </Link>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <Link href="/detail/2" prefetch={false}>
+                    Go to details id=2
+                </Link>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <Link href="/pokemon/pikachu">Go to Pokemon</Link>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <Link href="/">Go to homepage</Link>
+            </div>
+            <button onClick={() => dispatch(fetchDetail(pageId))}>Refresh timestamp</button>
+        </>
+    );
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(store => async ({params}) => {
+    const id = params?.id;
+    if (!id || Array.isArray(id)) {
+        throw new Error('Param id must be a string');
+    }
+
+    await store.dispatch(fetchDetail(id));
+
+    console.log('State on server', store.getState());
+
+    return {
+        props: {
+            serverTimestamp: new Date().getTime(),
+        },
+    };
+});
+
+export default Page;
